23967c46ba55ed387ce6c5a2e1e5b5fc23b84706
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-20 15:14:32 +0530

    Update main.tf

--- Terraform/ec2/main.tf
+++ Terraform/ec2/main.tf
@@ -10,15 +10,15 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-2"
-  access_key = "AKIA52UAOGETYZUNT26A"
-  secret_key = "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"
+  region  = "ap-south-1"
+  access_key = "AKIASDFMEMBBIOKFHSUQ"
+  secret_key = "gNqDubs4GNhAYHgjK/4Vf/Cj3spHz2RXpguaFCXS  "
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0a5588cee1fe39fff"
+  ami           = "ami-0763cf792771fe1bd"
   instance_type = "t2.micro"
-  key_name = "test"
+  key_name = "krishnakeypair"
   security_groups = ["launch-wizard-1"]
   tags = {
     Name = "ExampleAppServerInstance"
