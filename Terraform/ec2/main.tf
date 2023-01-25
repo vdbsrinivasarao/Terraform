9becdab51df180151f14ebf1c0ba5c6f25457cbd
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2023-01-25 15:24:35 +0530

    Update main.tf

--- Terraform/ec2/main.tf
+++ Terraform/ec2/main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region  = "ap-south-1"
-  access_key = "AKIASDFMEMBBIOKFHSUQ"
-  secret_key = "gNqDubs4GNhAYHgjK/4Vf/Cj3spHz2RXpguaFCXS  "
+  access_key = ""
+  secret_key = " "
 }
 
 resource "aws_instance" "example" {
