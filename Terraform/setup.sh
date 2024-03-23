d5e14bee2a0a2622408bb7b6197d5503f4b403b3
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2024-03-23 20:05:19 +0530

    Update setup.sh

--- Terraform/setup.sh
+++ Terraform/setup.sh
@@ -4,4 +4,4 @@ sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
-export PATH="/usr/local/bin:$PATH"
+
